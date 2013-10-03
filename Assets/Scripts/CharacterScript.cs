8a3d20b57657748519b2ec3c9d3d3fe050bde0cc
duckys <duc@animeforfans.com>
2013-10-03 01:33:43 -0700

    Prepared for item implementation

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class CharacterScript : MonoBehaviour {
 	
 	public float movementSpeed = 10.0f;
-	public float jumpSpeed = 5.0f;
-	public float gravitySpeed = 10f;
+	public float jumpSpeed = 10.0f;
+	public float gravitySpeed = 1f;
 	public float maxSpeed = 10.0f;
 	public float airFactor = .5f;
+	public string horizontalAxis = "Horizontal1";
+	public string jumpAxis = "Jump1";
+	private string itemName;
 	private Vector3 moveDirection = Vector3.zero;
 	private CharacterController controller;
 
@@ -22,20 +25,20 @@ public class CharacterScript : MonoBehaviour {
 	void Update () {
 		
 		if(controller.isGrounded) {
-				moveDirection = new Vector3(Input.GetAxis("Horizontal1"), 0, 0);
-				moveDirection = transform.TransformDirection(moveDirection);
-				moveDirection *= movementSpeed;
-				
-				if(Input.GetButton("Jump1")) {
-					moveDirection.y = jumpSpeed;
-				}      
+			moveDirection = new Vector3(Input.GetAxis(horizontalAxis), 0, 0);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= movementSpeed;
+						
+			if(Input.GetButtonDown(jumpAxis)) {
+				moveDirection.y = jumpSpeed;
+
 			}
+		}
 			
-			else {
-				moveDirection.x += Input.GetAxis("Horizontal1") * airFactor;
-			}
-			
-		
+		else {
+			moveDirection.x += Input.GetAxis(horizontalAxis) * airFactor;
+		}
+
 		moveDirection.y -= gravitySpeed * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 		
@@ -47,5 +50,14 @@ public class CharacterScript : MonoBehaviour {
 			moveDirection.x = -maxSpeed;
 		}	
 		
+	}
+	
+	void OnTriggerEnter(Collider c) {
+		equipItem(c.gameObject.name);
+		Destroy(c.gameObject);
+	}
+
+	
+	void equipItem(string itemName) {
 	}
 }
