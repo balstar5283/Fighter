beccb7524f87b5a81a5fcba1cdc099beb6a364fa
duckys <duc@animeforfans.com>
2013-10-03 00:04:09 -0700

    Added air movement

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -6,7 +6,9 @@ public class CharacterScript : MonoBehaviour {
 	public float movementSpeed = 10.0f;
 	public float jumpSpeed = 5.0f;
 	public float gravitySpeed = 10f;
-	private Vector3 moveDirection;
+	public float maxSpeed = 10.0f;
+	public float airFactor = .5f;
+	private Vector3 moveDirection = Vector3.zero;
 	private CharacterController controller;
 
 
@@ -19,17 +21,31 @@ public class CharacterScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= movementSpeed;
+		if(controller.isGrounded) {
+				moveDirection = new Vector3(Input.GetAxis("Horizontal1"), 0, 0);
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= movementSpeed;
+				
+				if(Input.GetButton("Jump1")) {
+					moveDirection.y = jumpSpeed;
+				}      
+			}
 			
-			if(Input.GetButton("Jump")) {
-				moveDirection.y = jumpSpeed;
-			}      
-		}
+			else {
+				moveDirection.x += Input.GetAxis("Horizontal1") * airFactor;
+			}
+			
 		
 		moveDirection.y -= gravitySpeed * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
+		
+		if(moveDirection.x > maxSpeed) {
+			moveDirection.x = maxSpeed;
+		}
+		
+		if(moveDirection.x < -maxSpeed) {
+			moveDirection.x = -maxSpeed;
+		}	
+		
 	}
 }
