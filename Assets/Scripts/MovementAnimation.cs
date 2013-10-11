59ad2b71fc44a2de91c9d45ebc7d4dd20d0ad065
Tan <tandngu@gmail.com>
2013-10-10 17:53:11 -0700

    Updated MovementAnimation.cs

--- Assets/Scripts/MovementAnimation.cs
+++ Assets/Scripts/MovementAnimation.cs
@@ -5,9 +5,14 @@ public class MovementAnimation : MonoBehaviour {
 	
 	public int currentFrameCount;
 	public int currentFrameIndex;
-	public float characterDirectionOffset = 0;
 	public Vector3[] currentAnimation;
+	public Vector3[] currentAttackAnimation;
+	public string currentAnimationString;
 	
+	//Used to reference the gameobject which needs to be notified of events
+	public GameObject callback;
+	
+	public bool hideAttackArm = false;
 	/*
 	 * Animation data for walk, idle, jump
 	 */
@@ -19,12 +24,57 @@ public class MovementAnimation : MonoBehaviour {
 		new Vector3(.75f, .375f, 7),
 		new Vector3(.875f, .375f, 7)
 	};
-	
 	public Vector3[] idle = {
 		new Vector3(0, .375f, 15),
 		new Vector3(.125f, .375f, 15),
 		new Vector3(.250f, .375f, 15)
 	};
+	public Vector3[] jump = {
+		new Vector3(0, .125f, 5),
+		new Vector3(.125f, .125f, 1000)
+	};
+	public Vector3[] victory = {
+		new Vector3(.375f, .125f, 1000)
+	};
+	public Vector3[] defeat = {
+		new Vector3(.25f, 0, 40),
+		new Vector3(.375f, 0, 1000)
+	};
+	
+	/*
+	 * Animation data for punch, gunshot, jump kicking, and swinging bat
+	 */
+	public Vector3[] punch = {
+		new Vector3(0, .875f, 5),
+		new Vector3(.125f, .875f, 5),
+		new Vector3(.250f, .875f, 9),
+		new Vector3(.125f, .875f, 5),
+		new Vector3(0, .875f, 5)
+	};
+	public Vector3[] kick = {
+		new Vector3(.25f, .125f, 1000)
+	};
+	public Vector3[] displayGun = {
+		new Vector3(.375f, .875f, 1000)
+	};
+	public Vector3[] gunFire = {
+		new Vector3(.5f, .875f, 5),
+		new Vector3(.625f, .875f, 5),
+		new Vector3(.75f, .875f, 5)
+	};
+	public Vector3[] displayBat = {
+		new Vector3(0, .75f, 1000)
+	};
+	public Vector3[] swingBat = {
+		new Vector3(.125f, .75f, 7),
+		new Vector3(.25f, .75f, 7),
+		new Vector3(.375f, .75f, 7)
+	};
+	public Vector3[] blank = {
+		new Vector3(.875f, .875f, 1000)
+	};
+	
+	
 	
 	public enum PlayType {LOOP, PLAYONCE, CLAMP, ZIGZAG};
 	public PlayType currentPlaytype;
@@ -33,13 +83,14 @@ public class MovementAnimation : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		playIdle();
-		//faceRight();
+		playAnimation("idle");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.anyKeyDown) {
+			playAnimation("kick");
+		}
 		//Reverse animation
 		if(reverseAnimation) {
 			
@@ -52,6 +103,10 @@ public class MovementAnimation : MonoBehaviour {
 					break;
 				case PlayType.ZIGZAG:
 					currentFrameIndex = 0;
+					reverseAnimation = false;
+					break;
+				case PlayType.PLAYONCE:
+					playAnimation("blank");
 					reverseAnimation = false;
 					break;
 				default:
@@ -67,7 +122,7 @@ public class MovementAnimation : MonoBehaviour {
 			return;
 		}
 		//Regular animation
-		if(currentFrameCount >= currentAnimation[currentFrameIndex].z){
+		if (currentFrameCount >= currentAnimation[currentFrameIndex].z){
 			currentFrameIndex++;
 			if(currentFrameIndex >= currentAnimation.Length) {
 				switch (currentPlaytype) {
@@ -78,6 +133,12 @@ public class MovementAnimation : MonoBehaviour {
 					currentFrameIndex = currentAnimation.Length -1;
 					reverseAnimation = true;
 					break;
+				case PlayType.PLAYONCE:
+					playAnimation("blank");
+					break;
+				case PlayType.CLAMP:
+					currentFrameIndex--;
+					break;
 				default:
 					currentFrameIndex--;
 					break;
@@ -87,29 +148,83 @@ public class MovementAnimation : MonoBehaviour {
 			updateFrame();
 		}
 		currentFrameCount++;
-	}
-	
-	public void playWalk(){
-		currentFrameCount = 0;
-		currentFrameIndex = 0;
-		currentAnimation = walk;
-		currentPlaytype = PlayType.LOOP;
-		updateFrame();
-	}
-	
-	public void playIdle() {
-		currentFrameCount = 0;
-		currentFrameIndex = 0;
-		currentAnimation = idle;
-		currentPlaytype = PlayType.ZIGZAG;
-		updateFrame();
 		
 	}
 	
+	public void playAnimation(string animationType) {
+		reverseAnimation = false;
+		currentFrameCount = 0;
+		currentFrameIndex = 0;
+		currentAnimationString = animationType;
+		switch (animationType) {
+		case "walk":
+			currentAnimation = walk;
+			currentPlaytype = PlayType.LOOP;
+			break;
+		case "idle":
+			currentAnimation = idle;
+			currentPlaytype = PlayType.ZIGZAG;
+			break;
+		case "jump":
+			currentAnimation = jump;
+			currentPlaytype = PlayType.CLAMP;
+			break;
+		case "victory":
+			currentAnimation = victory;
+			currentPlaytype = PlayType.CLAMP;
+			break;
+		case "defeat":
+			currentAnimation = defeat;
+			currentPlaytype = PlayType.CLAMP;
+			break;
+		case "punch":
+			currentAnimation = punch;
+			currentPlaytype = PlayType.PLAYONCE;
+			break;
+		case "kick":
+			currentAnimation = kick;
+			currentPlaytype = PlayType.CLAMP;
+			break;
+		case "gunFire":
+			currentAnimation = gunFire;
+			currentPlaytype = PlayType.PLAYONCE;
+			break;
+		case "swingBat":
+			currentAnimation = swingBat;
+			currentPlaytype = PlayType.PLAYONCE;
+			break;
+		case "blank":
+			if (callback != null) {
+				callback.SendMessage("playAnimationDone", currentAnimationString);
+			}
+			currentAnimation = blank;
+			currentPlaytype = PlayType.CLAMP;
+			break;
+		case "displayGun":
+			currentAnimation = displayGun;
+			currentPlaytype = PlayType.CLAMP;
+			break;
+		case "displayBat":
+			currentAnimation = displayBat;
+			currentPlaytype = PlayType.CLAMP;
+			break;
+		default:
+			currentAnimation = idle;
+			currentPlaytype = PlayType.ZIGZAG;
+			break;
+		}
+		
+		updateFrame();
+	}
+	
 	void updateFrame(){
-		renderer.material.SetTextureOffset("_MainTex",
-			new Vector2(currentAnimation[currentFrameIndex].x,
-						currentAnimation[currentFrameIndex].y + characterDirectionOffset));
+		Vector2 offset = new Vector2(
+			currentAnimation[currentFrameIndex].x,
+			currentAnimation[currentFrameIndex].y);
+		if (hideAttackArm) {
+			offset.y -= .125f;
+		}
+		renderer.material.SetTextureOffset("_MainTex", offset);
 	}
 	
 	public void faceLeft() {
@@ -129,5 +244,13 @@ public class MovementAnimation : MonoBehaviour {
 					gameObject.transform.localScale.z);
 			
 		}
+	}
+	
+	public void showBackArm() {
+		hideAttackArm = false;
+	}
+	
+	public void hideBackArm() {
+		hideAttackArm = true;
 	}
 }
