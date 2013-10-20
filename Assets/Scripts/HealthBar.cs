8012f531486ae351b0d419ce5dc92b8873bcd112
Tan <tandngu@gmail.com>
2013-10-19 23:31:47 -0700

    Change HP meter

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,6 +8,8 @@ public class HealthBar : MonoBehaviour {
 	public int maxHealth = 100;
 	public int player1Health = 100;
 	public int player2Health = 100;
+	public GameObject player1Display;
+	public GameObject player2Display;
 	
 	private GameObject timer;
 	private Timer t;
@@ -16,11 +18,15 @@ public class HealthBar : MonoBehaviour {
 	void Start () {
 		timer = GameObject.Find ("Timer");
 		t = (Timer)timer.GetComponent(typeof(Timer));
+		player1Health = maxHealth;
+		player2Health = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		player1Display.renderer.material.SetTextureOffset("_MainTex", new Vector2(.5f * ((maxHealth - player1Health)/(float)maxHealth), 0));
+		player2Display.renderer.material.SetTextureOffset("_MainTex", new Vector2(.5f * ((maxHealth - player2Health)/(float)maxHealth), 0));
+	/*
 		if (Input.GetKeyDown (KeyCode.Z))
 		{
 			 gameObject.SendMessage("ApplyDamageToPlayer1", 50.0F);
@@ -29,12 +35,12 @@ public class HealthBar : MonoBehaviour {
 		{
 			 gameObject.SendMessage("ApplyDamageToPlayer2", 50.0F);
 		}
-	
+	*/
 	}
 	
 	void OnGUI() {
 		
-		
+		/*
 		GUI.Box (new Rect(20, 20, Screen.width / 3 / (maxHealth/maxHealth), 50),player1Health + "/" + maxHealth);
 		
 		if(player1Health > 0)
@@ -49,7 +55,7 @@ public class HealthBar : MonoBehaviour {
 			float offset = Screen.width / 3 / (maxHealth/maxHealth) - Screen.width / 3 / (maxHealth/ (float)player2Health);
 			GUI.Box (new Rect( (Screen.width-20-Screen.width / 3 / (maxHealth/maxHealth))+offset, 20, (Screen.width / 3 / (maxHealth/ (float)player2Health)), 50), "");
 		}
-		
+		*/
 	}
 	
 	public void ApplyDamageToPlayer1(int damage) {
